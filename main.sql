2835ea9e9b95adb0739ba0c2a10db3d398a9df79
jishnuck663 <68513265+jishnuck663@users.noreply.github.com>
2020-11-06 18:10:38 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,13 +1,4 @@
-/******************************************************************************
 
-                        Online SQLite Query Runner.
-                Code, Compile, Run and Debug SQLite query online.
-Write your query in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
-
-/* Enter your sql queries here */
 CREATE TABLE student(
         sno INTEGER,
         name CHAR(20),
@@ -28,7 +19,7 @@ ALTER TABLE student
 RENAME TO students;
 
 INSERT INTO students
-VALUES(1,'NANDANA','CSE',20);
+VALUES(1,'JISHNU','CSE',21);
 
 TRUNCATE students;
 
